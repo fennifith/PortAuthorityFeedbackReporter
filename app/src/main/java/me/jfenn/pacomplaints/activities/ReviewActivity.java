2b34869f34830006baeffb178dff6299a1c392c2
James Fenn <18jafenn90@gmail.com>
2019-05-22 20:40:51 -0400

    androidx

--- app/src/main/java/me/jfenn/pacomplaints/activities/ReviewActivity.java
+++ app/src/main/java/me/jfenn/pacomplaints/activities/ReviewActivity.java
@@ -2,10 +2,10 @@ package me.jfenn.pacomplaints.activities;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.BottomSheetDialog;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
